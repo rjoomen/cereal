d92a4c98a24a3de5c1c5018d8dfac8310af9f216
Roelof Oomen <rj.oomen@isohorti.com>
2025-12-02 08:53:04 +0100

    Fix warning (treated as error) for unused-variable

--- sandbox/sandbox.cpp
+++ sandbox/sandbox.cpp
@@ -637,6 +637,7 @@ int main()
     iar( vec2 );
     for( auto & v : vec2 )
     {
+      (void) v;
       assert( v[0] == "hey" );
       assert( v[1] == "there" );
       assert( v[2] == "buddy" );
